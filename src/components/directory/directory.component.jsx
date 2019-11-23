fdfa0e6c49964ed4202b73cf95781aae46d8e796
Erik Sandoval <erik.sandoval@icloud.com>
2019-11-23 13:02:22 -0800

    looping through state for directory feed.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory.styles.scss";
+
 class Directory extends Component {
   constructor() {
     super();
@@ -42,6 +45,11 @@ class Directory extends Component {
   }
   render() {
     return (
+      <div className='directory-menu'>
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
     );
   }
 }
