4a7bd813cf1b3182484c4130017391b81eb03c97
Erik Sandoval <erik.sandoval@icloud.com>
2019-11-23 20:16:21 -0800

    Created sign in and sign up component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./pages/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/sign-in' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
