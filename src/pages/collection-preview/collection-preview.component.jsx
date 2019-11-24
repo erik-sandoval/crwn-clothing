2d582814451bf5109183eb8e2946822c8cd1996e
Erik Sandoval <erik.sandoval@icloud.com>
2019-11-23 16:32:26 -0800

    collection-preview component created

--- src/pages/collection-preview/collection-preview.component.jsx
+++ src/pages/collection-preview/collection-preview.component.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-import "./collection-preview.style.scss";
+import CollectionItem from "../collection-item/collection-item.component";
+
+import "./collection-preview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className='collection-preview'>
       <h1 className='title'>{title.toUpperCase()}</h1>
       <div className='preview'>
-        {items.filter((item, i) => i <4 ).map(item => (
-          <div key={item.id}>{item.name}</div>
-        ))}
+        {items
+          .filter((item, i) => i < 4)
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps}></CollectionItem>
+          ))}
       </div>
     </div>
   );
