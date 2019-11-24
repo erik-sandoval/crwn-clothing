34b64d64a6c729a6c079274fb770f40ed6b403d7
Erik Sandoval <erik.sandoval@icloud.com>
2019-11-23 19:42:49 -0800

    Created header component

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import "./App.css";
 // components
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./pages/header/header.component";
 
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
