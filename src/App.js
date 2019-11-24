9788214c7c08c7a3c5a35c697297575dfb6fa1cf
Erik Sandoval <erik.sandoval@icloud.com>
2019-11-23 16:15:32 -0800

    created shop component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import "./App.css";
 
 // components
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div className='App'>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../collection-preview/collection-preview.component";
+
+class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...OtherCollectionProps }) => (
+          <CollectionPreview key={id} {...OtherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
