c099ffacd879ec94d3b4e69e7745eefdf9353e25
Erik Sandoval <erik.sandoval@icloud.com>
2019-11-26 14:25:08 -0800

    selectCartitems selector implemented into CartDropDown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import CartItem from "../cart-item/cart-item.component";
 import CustomButton from "../custom-button/custom-button.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 import "./cart-dropdown.styles.scss";
-import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = ({ cartItems }) => {
   return (
@@ -18,9 +19,8 @@ const CartDropdown = ({ cartItems }) => {
     </div>
   );
 };
-
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
