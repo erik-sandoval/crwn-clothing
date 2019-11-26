1c218c2ccb3d5c93a0b18b8e36df2d2e996328cf
Erik Sandoval <erik.sandoval@icloud.com>
2019-11-25 22:06:03 -0800

    Created CartIcon component and implemented into Header

--- src/pages/header/header.component.jsx
+++ src/pages/header/header.component.jsx
@@ -6,6 +6,7 @@ import { connect } from "react-redux";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
 
 const Header = ({ currentUser }) => {
   return (
@@ -29,6 +30,7 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
     </div>
   );
