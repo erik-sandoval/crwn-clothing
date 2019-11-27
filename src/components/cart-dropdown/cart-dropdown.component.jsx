622861fa934557b215a02bdb745c4d4bdea84d40
Erik Sandoval <erik.sandoval@icloud.com>
2019-11-26 16:38:32 -0800

    cart drop down disappears after going to checkout page.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,11 +5,13 @@ import { createStructuredSelector } from "reselect";
 
 import CartItem from "../cart-item/cart-item.component";
 import CustomButton from "../custom-button/custom-button.component";
+
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = ({ cartItems, history }) => {
+const CartDropdown = ({ cartItems, dispatch, history }) => {
   return (
     <div className='cart-dropdown'>
       <div className='cart-items'>
@@ -21,12 +23,18 @@ const CartDropdown = ({ cartItems, history }) => {
           <span className='empty-message'>Your cart is empty</span>
         )}
       </div>
-      <CustomButton onClick={() => history.push("/checkout")}>
+      <CustomButton
+        onClick={() => {
+          dispatch(toggleCartHidden());
+          history.push("/checkout");
+        }}
+      >
         GO TO CHECKOUT
       </CustomButton>
     </div>
   );
 };
+
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
 });
