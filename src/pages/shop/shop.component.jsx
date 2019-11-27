5191d5222cd1e172493a933323672313a101abc5
Erik Sandoval <erik.sandoval@icloud.com>
2019-11-27 01:03:28 -0800

    removed unnecessary files from shop component since I moved most of it CollectionsOverview component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,12 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import React from "react";
+import CollectionsOverview from "../collections-overview/collections-overview.component";
 
-import CollectionPreview from "../collection-preview/collection-preview.component";
-import { shopCollectionsSelector } from "../../redux/shop/shop.selectors";
-
-const ShopPage = ({ collections }) => {
+const ShopPage = () => {
   return (
     <div className='shop-page'>
-      {collections.map(({ id, ...OtherCollectionProps }) => (
-        <CollectionPreview key={id} {...OtherCollectionProps} />
-      ))}
+      <CollectionsOverview />
     </div>
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: shopCollectionsSelector
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
