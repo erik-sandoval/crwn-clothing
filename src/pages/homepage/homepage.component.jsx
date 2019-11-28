2ade4b97d85788bfae7c76026a457408e80b3879
Erik Sandoval <erik.sandoval@icloud.com>
2019-11-28 14:16:51 -0800

    converted Homepage to Styled components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import "./homepage.styles.scss";
+
 import Directory from "../../components/directory/directory.component";
+
+import { HomepageContainer } from "./homepage.styles";
 
 const HomePage = () => {
   return (
-    <div className='homepage'>
+    <HomepageContainer>
       <Directory></Directory>
-    </div>
+    </HomepageContainer>
   );
 };
 
