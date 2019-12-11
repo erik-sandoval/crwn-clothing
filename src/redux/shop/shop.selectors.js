9b305cda7f6457d5ae91e975daaa0aef90a7ef89
Erik Sandoval <erik.sandoval@icloud.com>
2019-12-10 21:14:15 -0800

    added new action types and modified reducer for new actions

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -17,3 +17,13 @@ export const selectCollection = collectionUrlParam =>
   createSelector([shopCollectionsSelector], collections =>
     collections ? collections[collectionUrlParam] : null
   );
+
+export const selectIsCollectionFetching = createSelector(
+  [shopSelector],
+  shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+  [shopSelector],
+  shop => !!shop.collections
+);
