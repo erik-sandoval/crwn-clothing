fcee4ced1c56ae4ea0e67713812b77dd50d523ed
Erik Sandoval <erik.sandoval@icloud.com>
2019-12-10 21:57:05 -0800

    created page Collections Overview Container and fixed mistake on other container

--- src/pages/collections-overview/collections-overview.container.jsx
+++ src/pages/collections-overview/collections-overview.container.jsx
@@ -2,18 +2,18 @@ import { connect } from "react-redux";
 import { compose } from "redux";
 import { createStructuredSelector } from "reselect";
 
-import CollectionPage from "./collection.component";
 import WithSpinner from "../../components/with-spinner/with-spinner.component";
+import CollectionsOverview from "./collections-overview.component";
 
-import { selectIsCollectionsLoaded } from "../../redux/shop/shop.selectors";
+import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
 
 const mapStateToProps = createStructuredSelector({
-  isLoading: state => !selectIsCollectionsLoaded(state)
+  isLoading: selectIsCollectionFetching
 });
 
-const CollectionPageContainer = compose(
+const CollectionsOverviewContainer = compose(
   connect(mapStateToProps),
   WithSpinner
-)(CollectionPage);
+)(CollectionsOverview);
 
-export default CollectionPageContainer;
+export default CollectionsOverviewContainer;
