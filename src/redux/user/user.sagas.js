9bffdbcda5fcdb84d03f0d3678238849b86c982a
Erik Sandoval <48698089+erik-sandoval@users.noreply.github.com>
2019-12-21 21:49:11 -0800

    created sagas for email and google sign in

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -1,28 +1,50 @@
-import { takeLatest, put } from "redux-saga/effects";
+import { takeLatest, put, all, call } from "redux-saga/effects";
 import UserActionTypes from "./user.types";
+import {
+  auth,
+  googleProvider,
+  createUserProfileDocument
+} from "../../firebase/firebase.utils";
+import { signInSuccess, signInFailure } from "./user.actions";
 
+export function* getSnapshotFromUserAuth(userAuth) {
+  try {
+    const userRef = yield call(createUserProfileDocument, userAuth);
 
-export function* onGoogleSignInSuccess() {
+    const userSnapshot = yield userRef.get();
 
+    yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+  } catch (error) {
+    yield put(signInFailure(error));
+  }
+}
+
+export function* onGoogleSignIn() {
+  try {
+    const { user } = yield auth.signInWithPopup(googleProvider);
+    yield getSnapshotFromUserAuth(user);
+  } catch (error) {
+    yield put(signInFailure(error));
+  }
 }
 
 export function* onGoogleSignInStart() {
-    yield 
+  yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, onGoogleSignIn);
 }
 
-// this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-//     if (userAuth) {
-//       const userRef = await createUserProfileDocument(userAuth);
+export function* onEmailSignIn({ payload: { email, password } }) {
+  try {
+    const { user } = yield auth.signInWithEmailAndPassword(email, password);
+    yield getSnapshotFromUserAuth(user);
+  } catch (error) {
+    yield put(signInFailure(error));
+  }
+}
 
-//       userRef.onSnapshot(snapShot => {
-//         setCurrentUser({
-//           currentUser: {
-//             id: snapShot.id,
-//             ...snapShot.data()
-//           }
-//         });
-//       });
-//     } else {
-//       setCurrentUser(userAuth);
-//     }
-//   });
+export function* onEmailSignInStart() {
+  yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, onEmailSignIn);
+}
+
+export function* userSagas() {
+  yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
+}
